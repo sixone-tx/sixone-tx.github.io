313785d8fc5373a65b61b2350c581301f071a8f1
litx <sixone.x@qq.com>
2021-04-18 17:06:45 +0800

    1

--- service-worker.js
+++ service-worker.js
@@ -59,7 +59,7 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log(event);
+  // console.log(event);
   // We only want to call event.respondWith() if this is a navigation request
   // for an HTML page.
   if (event.request.mode === "navigate") {
